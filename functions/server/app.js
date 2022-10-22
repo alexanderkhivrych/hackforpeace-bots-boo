dbab9061df9cddbb210a12a8ae9f51990e74bfc3
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-10-22 18:48:33 +0100

    test

--- functions/server/app.js
+++ functions/server/app.js
@@ -19,7 +19,7 @@ const getMatchScoresByUserNames = require("./getMatchScoresByUserNames");
   const routerBasePath = process.env.NODE_ENV !== 'production' ? `/${functionName}` : `/.netlify/functions/${functionName}/`
 
 
-  router.get('/check/users', async (req, res) => {
+  router.get('/users', async (req, res) => {
     const exampleBulkUserData = ["@SVecerinka", "@sasha_khivrych"];
      const botometerResults = await getMatchScoresByUserNames(exampleBulkUserData);
 
