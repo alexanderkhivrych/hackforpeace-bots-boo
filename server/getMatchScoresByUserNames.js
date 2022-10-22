9ecc73960c70883ce1b85d42e7869c621ef077b5
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-10-22 19:19:22 +0100

    refactor

--- server/getMatchScoresByUserNames.js
+++
@@ -1,9 +0,0 @@
-const getBotometrMatchScoreData = require("./getBotometrMatchScoreData")
-
-const getMatchScoresByUserNames = (usernames = []) =>
-  Promise
-    .all(usernames.map(getBotometrMatchScoreData))
-    .catch(err => console.log(err))
-
-
-module.exports = getMatchScoresByUserNames;
