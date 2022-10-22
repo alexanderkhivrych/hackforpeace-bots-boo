4769e0fff9280bf3c40359a24bc8bf79123bf17a
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-10-22 18:34:10 +0100

    test

--- functions/botsboo.js
+++ functions/botsboo.js
@@ -1,6 +1,6 @@
 /* example using https://github.com/dougmoscrop/serverless-http */
-import serverless from 'serverless-http'
-import expressApp from './server/app'
+const serverless = require('serverless-http');
+const expressApp = require('./server/app');
 
 // We need to define our function name for express routes to set the correct base path
 const functionName = 'botsboo'
