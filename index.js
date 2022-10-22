7bf77c26b672ea1ae360a193da0b1ddc7631fbcb
alexanderkhivrych <alexander.khivrych@gmail.com>
2022-10-22 15:59:45 +0100

    feat: added getMatchScoresByUserNames

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const { Botometer } = require("botometer");
+const exampleBulkUserData = ["@SVecerinka", "@sasha_khivrych"];
 
-const botometer = new Botometer({
-  consumerKey: "mgNyLdAfptsBWaPatkT4ikLbE",
-  consumerSecret: "G5woIPH1jPmLdy1J1QTLraSlDBSs3ois0EUQkVoEMh8QWurl9J",
-  accessToken: "701377153812791297-9FQ553Gm1TqsxXeYLotp2z7dNJrTb3c",
-  accessTokenSecret: "pdmZeVxA4hgEN42vmOouNdZNCGaKJ20vujWApCT66OeeP",
-  rapidApiKey: "82dbbc8ae6msh227f257d810b049p15dec8jsnee0be1a5dddb",
-  supressLogs: false, // Not required. Defaults to true
-  usePro: true,
-  returnTwitterData: false,
-});
+const getMatchScoresByUserNames = require("./server/getMatchScoresByUserNames");
 
+(async () => {
+  const botometerResults = await getMatchScoresByUserNames(exampleBulkUserData);
 
-async function run() {
-  const results = await botometer.getScores(["@SVecerinka", "@sasha_khivrych"]);
-
-  console.log(JSON.stringify(results));
-}
-
-run();
+  console.log(botometerResults);
+})()
